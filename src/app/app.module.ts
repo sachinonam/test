58b6a509a95b856ca4c526c98fea56edb61389d1
sachin <sachinonam@gmail.com>
2020-06-12 14:42:53 +0100

    new repo

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+declarations: [ AppComponent],
+imports: [ BrowserModule, AppRoutingModule, AmplifyUIAngularModule],
+providers: [],
+bootstrap: [AppComponent]
+})
+export class AppModule { }
