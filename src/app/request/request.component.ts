c3c950e09d9f7a28e0cfcfc6a19d1a7eea474bdf
Shruti Kothari <66637508+shkothari88@users.noreply.github.com>
2020-06-20 00:09:12 +0100

    Add files via upload

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { AmplifyService } from 'aws-amplify-angular';
 import { Router } from '@angular/router';
-
+import API from '@aws-amplify/api';
+import 'rxjs/add/operator/toPromise';
+import Amplify, { Auth } from 'aws-amplify';
+import { FormFieldTypes } from '@aws-amplify/ui-components';
 
 @Component({
   selector: 'app-request',
@@ -33,6 +36,23 @@ export class RequestComponent implements OnInit {
   }
 
   renderSeeker(): any {
+    userRequest();
     this.router.navigate(['/seeker']);
   }
+
 }
+  async function userRequest()
+  {
+    const apiName = 'testthis';
+     return await API.get(apiName, '/userrequest' , { headers: {'Content-Type': 'application/json', 'Authorization' : 'token'}})
+      .then(response => {
+        debugger;
+        console.log()
+          return `async response.data`;
+      })
+      .catch(error => {
+        debugger;
+        return error
+         console.log(error.message);
+     });
+    }
